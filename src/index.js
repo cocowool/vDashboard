0b53fefa512597065fb28d6f82132dacd179ed50
Green Wang <cocowool@qq.com>
2018-11-19 18:51:55 +0800

    Add img loader

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import _ from 'lodash'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
+import './main.css'
+import Icon from './icon_service.gif'
 
 function component() {
     let element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello', 'webpack!'], ' ');
+
+    var myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
 
     return element;
 }
