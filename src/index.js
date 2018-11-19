71793e8169c62b8d4acc213a139b5437928d93fe
Green Wang <cocowool@qq.com>
2018-11-19 19:16:28 +0800

    load data

--- src/index.js
+++ src/index.js
@@ -3,16 +3,19 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
 import './main.css'
 import Icon from './icon_service.gif'
+import Data from './bsuser_utf8.json'
 
 function component() {
     let element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello', 'webpack!'], ' ');
+    element.classList.add('vd-title')
 
     var myIcon = new Image();
     myIcon.src = Icon;
 
-    element.appendChild(myIcon);
+    // element.appendChild(myIcon);
+    console.log(Data);
 
     return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,10 @@ module.exports = {
     module: {
         rules: [
             { test: /\.css$/, use: ['style-loader', 'css-loader']},
-            { test: /\.(png|svg|jpg|gif)$/, use: ['file-loader']}
+            { test: /\.(png|svg|jpg|gif)$/, use: ['file-loader']},
+            { test: /\.(woff|woff2|eot|ttf|otf)$/, use: ['file-loader']},
+            { test: /\.(csv|tsv)$/, use: ['csv-loader']},
+            { test: /\.xml$/, use: ['xml-loader'] }
         ]
     }
 };
