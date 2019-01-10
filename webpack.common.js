dc9519987bd6c5d51d5d2d576256b584d69de121
Green Wang <cocowool@qq.com>
2019-01-10 19:47:39 +0800

    The second code submit at 2019

--- webpack.common.js
+++ webpack.common.js
@@ -56,7 +56,8 @@ module.exports = {
             { test: /\.(png|svg|jpg|gif)$/, use: ['file-loader']},
             { test: /\.(woff|woff2|eot|ttf|otf)$/, use: ['file-loader']},
             { test: /\.(csv|tsv)$/, use: ['csv-loader']},
-            { test: /\.xml$/, use: ['xml-loader'] }
+            { test: /\.xml$/, use: ['xml-loader'] },
+            { test: /\.scss$/, use: ['sass-loader'] }
         ]
     }
 };
